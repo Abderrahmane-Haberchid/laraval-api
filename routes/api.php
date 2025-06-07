01e1a07ab5159d039c05a68f6c3cfc10141b5c83
abderahman <jhonsnowstark2@gmail.com>
2025-06-07 12:26:32 +0100

    using apiResource to interact with articles

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,3 @@
 <?php
 
-Route::get('/articles', [App\Http\Controllers\Api\ArticleController::class, 'index']);
-Route::get('/articles/{id}', [App\Http\Controllers\Api\ArticleController::class, 'show']);
-Route::post('/articles', [App\Http\Controllers\Api\ArticleController::class, 'store']);
-Route::put('/articles/{id}', [App\Http\Controllers\Api\ArticleController::class, 'update']);
-Route::delete('/articles/{id}', [App\Http\Controllers\Api\ArticleController::class, 'delete']);
+Route::apiResource('articles', ArticleController::class);
